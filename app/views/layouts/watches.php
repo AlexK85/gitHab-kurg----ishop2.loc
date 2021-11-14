fcdac96da134771c4cbb9589de5c67aa79ed5cc8
AlexK85 <saharr.k@mail.ru>
2021-11-15 01:09:01 +0300

    Добавил скрипты для работы табов

--- app/views/layouts/watches.php
+++ app/views/layouts/watches.php
@@ -241,7 +241,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	<script src="js/jquery.easydropdown.js"></script>
 
 	<!--Slider-Starts-Here-->
-	<script src="js/responsiveslides.min.js"></script>
+
 	<script>
 		// You can also use "$(window).load(function() {"
 		$(function() {
@@ -277,6 +277,32 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			});
 		});
 	</script>
+
+
+	<script src="js/jquery.easydropdown.js"></script>	
+	<script type="text/javascript">
+		$(function() {
+		
+			var menu_ul = $('.menu_drop > li > ul'),
+				menu_a  = $('.menu_drop > li > a');
+			
+			menu_ul.hide();
+		
+			menu_a.click(function(e) {
+				e.preventDefault();
+				if(!$(this).hasClass('active')) {
+					menu_a.removeClass('active');
+					menu_ul.filter(':visible').slideUp('normal');
+					$(this).addClass('active').next().stop(true,true).slideDown('normal');
+				} else {
+					$(this).removeClass('active');
+					$(this).next().stop(true,true).slideUp('normal');
+				}
+			});
+		
+		});
+	</script>	
+	
 
 	<script src="js/main.js"></script>
 	<!--End-slider-script-->
